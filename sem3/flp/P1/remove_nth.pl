071da7bd89575dfcd5b582435e8f290a1bce7a00
Damocles <38077049+damoc1es@users.noreply.github.com>
2022-10-25 17:05:38 +0300

    fixed prolog predicates not being determinist

--- sem3/flp/P1/remove_nth.pl
+++ sem3/flp/P1/remove_nth.pl
@@ -7,16 +7,21 @@ remove_nth(l1 l2 .. ln, N) =  |  []                             , n = 0
 */
 
 % remove_nth(L: list, N: integer, Rez: list)
-%           (i, i, o)
+%           (i, i, o) (i, i, i) determinist
+% L : lista din care se sterge elementul
+% N : pozitia care se sterge
+% Rez : lista rezultanta
+
+% daca lista e goala, returnez lista goala
 remove_nth([], _, []).
 
-
+% daca s-a ajuns la N = 1, returnez lista fara primul element
 remove_nth([_|T], N, T) :-
-    1 is N.
+    N = 1, !.
 
-
+% daca nu s-a ajuns la N = 1, iau primul element si il adaug la rezolvare
 remove_nth([H|T], N, [H|R]) :-
-    not(1 is N),
+    N \= 1,
     N1 is N-1,
     remove_nth(T, N1, R).
 
@@ -24,9 +29,9 @@ remove_nth([H|T], N, [H|R]) :-
 % run_tests.
 :- begin_tests(remove_nth).
 
-test("first position", [nondet]) :- remove_nth([7,8,9], 1, [8,9]).
-test("middle position", [nondet]) :- remove_nth([7,8,9], 2, [7,9]).
-test("end position", [nondet]) :- remove_nth([7,8,9], 3, [7,8]).
-test("non-existent position", [nondet]) :- remove_nth([7,8,9], 4, [7,8,9]).
+test("first position") :- remove_nth([7,8,9], 1, [8,9]).
+test("middle position") :- remove_nth([7,8,9], 2, [7,9]).
+test("end position") :- remove_nth([7,8,9], 3, [7,8]).
+test("non-existent position") :- remove_nth([7,8,9], 4, [7,8,9]).
 
 :- end_tests(remove_nth).
--- sem3/flp/P1/substitute_element.pl
+++ sem3/flp/P1/substitute_element.pl
@@ -1,61 +1,70 @@
 /*
-my_append(l1 l2 .. ln, r1 r2 .. rm) = | l1 l2 .. ln                         , m = 0
-                                      | r1 ⊕ my_append(l1 .. ln, r2 .. rm) , altfel
+Sa se scrie un predicat care substituie intr-o lista un element printr-o alta lista.
+
+substitute_elem(l1 l2 .. ln, K, r1 r2 .. rm) = | []                                                                , n = 0
+                                               | my_append(r1 r2 .. rm, substitute_elem(l2 .. ln, K, r1 r2 .. rm)) , l1 = K
+                                               | l1 ⊕ substitute_elem(l2 .. ln, K, r1 r2 .. rm)                   , altfel
+
+my_append(l1 l2 .. ln, r1 r2 .. rm) = | r1 r2 .. rm                         , n = 0
+                                      | l1 ⊕ my_append(l2 .. ln, r1 .. rm) , altfel
 my_append(L, R) = L ⊕ R
 */
 
 % my_append(L1: list, L2: list, LRez: list)
-%          (i, i, o)
+%          (i, i, o) (i, i, i) determinist
+% L1 : lista care se adauga in stanga lui L2
+% L2 : lista din dreapta
+
+% daca L1 este vid, am terminat concatenarea
 my_append([], T, T).
 
-
+% se tot adauga primul element al primei liste la a doua lista
 my_append([H1|T1], T2, [H1|L]) :-
     my_append(T1, T2, L).
 
 
 :- begin_tests(my_append).
 
-test("1", [nondet]) :- my_append([], [1, 2], [1, 2]).
-test("2", [nondet]) :- my_append([1, 2], [], [1, 2]).
-test("3", [nondet]) :- my_append([], [], []).
-test("4", [nondet]) :- my_append([1, 2], [3, 4], [1, 2, 3, 4]).
+test("1") :- my_append([], [1, 2], [1, 2]).
+test("2") :- my_append([1, 2], [], [1, 2]).
+test("3") :- my_append([], [], []).
+test("4") :- my_append([1, 2], [3, 4], [1, 2, 3, 4]).
 
 :- end_tests(my_append).
 
-/*
-Sa se scrie un predicat care substituie intr-o lista un element printr-o alta lista.
-
-substitute_elem(l1 l2 .. ln, K, r1 r2 .. rn) = | []                                                       , n = 0
-                                               | r1 r2 .. rn ⊕ substitute_elem(l2 .. ln, K, r1 r2 .. rn) , l1 = K
-                                               | l1 ⊕ substitute_elem(l2 .. ln, K, r1 r2 .. rn)          , altfel
-*/
 
 % substitute_element(L1: list, K: element, L2: list, LRez)
-%                   (i, i, i, o)
+%                   (i, i, i, o) (i, i, i, i) determinist
+% L1 : lista din care se substituie
+% K : elementul care este inlocuit
+% L2 : lista care substituie elementul K
+% LRez : lista rezultanta
+
+% daca lista este goala, returnez lista
 substitute_element([], _, _, []).
 
-
+% daca primul element din lista este K, concatenez la continuarea rezolvarii
 substitute_element([H|T], K, L, R2) :-
-    H is K,
+    H = K,
     substitute_element(T, K, L, R),
-    my_append(L, R, R2).
+    my_append(L, R, R2), !.
 
-
+% daca primul element != K, iau primul element si il adaug la rezolvare
 substitute_element([H|T], K, L, [H|R]) :-
-    not(H is K),
+    H \= K,
     substitute_element(T, K, L, R).
 
 
 % run_tests.
 :- begin_tests(substitute_element).
 
-test("1", [nondet]) :- substitute_element([], 8, [1, 2], []).
-test("2", [nondet]) :- substitute_element([7, 8, 9], 8, [1, 2], [7, 1, 2, 9]).
-test("3", [nondet]) :- substitute_element([7, 8, 9], 8, [1], [7, 1, 9]).
-test("4", [nondet]) :- substitute_element([7, 8, 9], 8, [], [7, 9]).
-test("5", [nondet]) :- substitute_element([7, 8, 9], 7, [1, 2], [1, 2, 8, 9]).
-test("6", [nondet]) :- substitute_element([7, 8, 9], 9, [1, 2], [7, 8, 1, 2]).
-test("7", [nondet]) :- substitute_element([7, 8, 9], 6, [1, 2], [7, 8, 9]).
-test("8", [nondet]) :- substitute_element([1, 1, 2, 3, 2, 5], 2, [7, 8], [1, 1, 7, 8, 3, 7, 8, 5]).
+test("1") :- substitute_element([], 8, [1, 2], []).
+test("2") :- substitute_element([7, 8, 9], 8, [1, 2], [7, 1, 2, 9]).
+test("3") :- substitute_element([7, 8, 9], 8, [1], [7, 1, 9]).
+test("4") :- substitute_element([7, 8, 9], 8, [], [7, 9]).
+test("5") :- substitute_element([7, 8, 9], 7, [1, 2], [1, 2, 8, 9]).
+test("6") :- substitute_element([7, 8, 9], 9, [1, 2], [7, 8, 1, 2]).
+test("7") :- substitute_element([7, 8, 9], 6, [1, 2], [7, 8, 9]).
+test("8") :- substitute_element([1, 1, 2, 3, 2, 5], 2, [7, 8], [1, 1, 7, 8, 3, 7, 8, 5]).
 
 :- end_tests(substitute_element).
